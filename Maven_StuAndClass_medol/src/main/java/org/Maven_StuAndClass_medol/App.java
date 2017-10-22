13980451a5cff93305faef715cd17aebb9845e43
qjt <qjt@qq.com>
2017-10-22 20:05:02 +0800

    commit Maven_StuAndClass

---
+++ Maven_StuAndClass_medol/src/main/java/org/Maven_StuAndClass_medol/App.java
@@ -0,0 +1,13 @@
+package org.Maven_StuAndClass_medol;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
